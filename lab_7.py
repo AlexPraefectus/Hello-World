ea9bf97e079fbc98d112eda62c4e4087a16db440
AlexPraefectus <21339807+AlexPraefectus@users.noreply.github.com>
2017-12-17 11:16:20 +0300

    lab_7
    final

--- lab_7.py
+++ lab_7.py
@@ -1,12 +1,30 @@
 import os.path
 import re
+import os
+import pickle
+import shutil
+import shelve
+import time
+
+
+countries_dict = {
+                'Spain': (505990, 46528966),  # 91
+                'Monaco': (2.02, 37550),  # 18589.90
+                'Ukraine': (576500, 42456012),  # 73
+                'Netherlands': (41526, 17165895),  # 413
+}
+dictionary = {
+              'France': (543965, 67135000),  # 123
+              'Belgium': (30528, 11370968),  # 372
+              'Italy': (301308, 60525277)  # 201
+}
 
 
 cfg = {'encoding': 'utf-8',
-       'path': '10.txt',
+       'path': 'D:\\10.txt',
        'errors': 'ignore',
-       'file_1_path': '1.txt',
-       'file_2_path': '2.txt',
+       'file_1_path': 'D:\\lab7\\1.txt',
+       'file_2_path': 'D:\\lab7\\2.txt',
        }
 
 
@@ -50,7 +68,7 @@ class TextProcessing:
         self.processing_text = text
         self.raw_sentences_lst1 = []
         self.raw_sentences_lst2 = []
-        self.final_sentences_lst = []
+        self.raw_sentences_lst3 = []
 
     def deleting_control_symbols(self):
         self.processing_text = self.processing_text.replace('\n', ' ')
@@ -92,12 +110,12 @@ class TextProcessing:
         for k in self.raw_sentences_lst2:
             try:
                 while k:
-                    res = re.search('(\w[\?\.!] ?\- \w|\w[\?\.!]{1} |\w\[[0-9]\]. )', k)
+                    res = re.search('(\w[\?\.!]{1} |\w\[[0-9]\]\.)', k)
                     if not res:
-                        self.final_sentences_lst.append(k)
+                        self.raw_sentences_lst3.append(k)
                         break
                     else:
-                        self.final_sentences_lst.append(k[:k.index(res.group(0)) + len(res.group(0))])
+                        self.raw_sentences_lst3.append(k[:k.index(res.group(0)) + len(res.group(0))])
                         k = k[k.index(res.group(0)) + len(res.group(0)):]
             except ValueError:
                 pass
@@ -107,7 +125,7 @@ class TextProcessing:
         self.split1_by_3dots()
         self.split2_by_quest_exclam_marks()
         self.split_by_single_marks()
-        return self.final_sentences_lst
+        return self.raw_sentences_lst3
 
     def get_sentence_with_odd_or_even_length(self, needed='odd'):
         lst = []
@@ -123,22 +141,42 @@ class TextProcessing:
         return lst
 
 
+try:
+    os.mkdir('D:\\lab7\\')
+    os.mkdir('D:\\lab7\\Korienev\\')
+    os.mkdir('D:\\lab5\\')
+    os.chdir('D:\\lab7\\')
+    with open('data.pickle', 'wb') as f:
+        pickle.dump(countries_dict, f)
+    shutil.move('data.pickle', 'D:\\lab5\\')
+    os.chdir('D:\\lab5\\')
+    with open('data.pickle', 'rb') as f:
+        data_new = pickle.load(f)
+        data_new.update(dictionary)
+    with open('new_data_pickle', 'wb') as f:
+        pickle.dump(data_new, f)
+    os.mkdir('D:\\lab6\\')
+    a = shelve.open('D:\\myShelve')
+    for i in countries_dict.keys():
+        a[i] = countries_dict[i]
+    a.close()
+    shutil.move('D:\\myShelve.dir', 'D:\\lab6\\')
+    shutil.move('D:\\myShelve.dat', 'D:\\lab6\\')
+    shutil.move('D:\\myShelve.bak', 'D:\\lab6\\')
+    os.rename('D:\\lab6\\myShelve.bak', 'D:\\lab6\\newfile.ext')
+    os.utime('D:\\lab6\\newfile.ext', (time.time(), time.time() - 20000))
+    os.chmod('D:\\lab6\\newfile.ext', mode=7)
+except ValueError:
+    print("OSError")
+
+
 i_file = FileControl(path=cfg['path'], mode='r')
 o1_file = FileControl(path=cfg['file_1_path'], mode='w')
+o2_file = FileControl(path=cfg['file_2_path'], mode='w')
 text_processor = TextProcessing(i_file.get_text())
 needed_sentences = text_processor.get_sentence_with_odd_or_even_length(needed='odd')
 for i in needed_sentences:
-    print(i.strip())
-
-
-"""
-if o2_file.opened_for_writing_flag:
-    for j in i_file.get_text().split():
-        if len(re.findall('[аoуiиеяюєїАОУІИЕЯЮЄЇ]', j)) >= 3:
-            o2_file.write_to_file_w_newline(re.search('[І-є]+', j))
-
-
-c = LabWork7(cfg['path'], cfg['file_1_path'], cfg['file_2_path'])
-c.task1()
-c.task2()
-"""
+    o1_file.write_to_file_w_newline(i.strip())
+for j in i_file.get_text().split():
+    if len(re.findall('[аoуiиеяюєїАОУІИЕЯЮЄЇ]', j)) >= 3:
+        o2_file.write_to_file_w_newline(j)
